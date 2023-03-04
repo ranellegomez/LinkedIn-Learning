f7b6dd78b2817f0601edb8b235b094a0ec2b9941
Ranelle Gomez <ranellegomez@berkeley.edu>
2023-03-04 14:36:03 -0800

    Added functional component

--- React Using TypeScript/react-typescript-webstorm/src/App.tsx
+++ React Using TypeScript/react-typescript-webstorm/src/App.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
 
 function App() {
-    // tuples are arrays that can take multiple types.
-    let myTuple: [string, number] = ['Ranelle', 56];
-    // enums: maps attribute names to numbers.
-    enum Codes {first, second, third};
-    // any. Use when you're not sure what type will be.
-    let firstName: any = 'Ranelle';
-    // void. use, for example, when you need to void the return type of function.
-    const warning = (): void => {
-        console.log("Warning")
-    }
 
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    The value {myTuple[0]} is of {typeof myTuple[0]} type!
-                    The value {myTuple[1]} is of {typeof myTuple[1]} type!
-                </p>
+                <Message></Message>
             </header>
         </div>
     );
---
+++ React Using TypeScript/react-typescript-webstorm/src/Message.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// functional component
+const Message = () => {
+    return (
+        <p>This is a quick message!</p>
+    );
+}
+
+export default Message;
