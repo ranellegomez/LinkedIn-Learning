65d5738b50a57a670f3b13fee4ebd7edd8bf0c3d
Ranelle Gomez <ranellegomez@berkeley.edu>
2023-03-04 15:11:46 -0800

    Class/state component syntax

--- React Using TypeScript/react-typescript-webstorm/src/App.tsx
+++ React Using TypeScript/react-typescript-webstorm/src/App.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Message from './Message';
 
-function App() {
+class App extends Component<any> {
+    // Runs before component is mounted.
+    UNSAFE_componentWillUpdate() {
+        console.log('Almost there...')
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <Message message = 'This is a simple message' />
-            </header>
-        </div>
-    );
+    }
+
+    // Runs after component mounts.
+    componentDidMount() {
+        console.log('Done.')
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <Message message='This is a simple message'/>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
