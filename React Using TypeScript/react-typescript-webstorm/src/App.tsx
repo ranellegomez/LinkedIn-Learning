ef916a1aa7b42e7bfa358820cd726f61f52fe2a9
Ranelle Gomez <ranellegomez@berkeley.edu>
2023-03-04 14:19:15 -0800

    Check if string type matches

--- React Using TypeScript/react-typescript-webstorm/src/App.tsx
+++ React Using TypeScript/react-typescript-webstorm/src/App.tsx
@@ -3,21 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let firstValue: string = 'Ranelle';
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+            The value {firstValue} is of {typeof firstValue} type!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
