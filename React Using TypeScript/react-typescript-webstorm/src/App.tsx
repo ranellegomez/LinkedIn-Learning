88daf44f1ef934dd536394dc500b9de3934a1683
Ranelle Gomez <ranellegomez@berkeley.edu>
2023-03-04 14:22:02 -0800

    Checking array type

--- React Using TypeScript/react-typescript-webstorm/src/App.tsx
+++ React Using TypeScript/react-typescript-webstorm/src/App.tsx
@@ -4,17 +4,19 @@ import './App.css';
 
 function App() {
     let firstValue: string = 'Ranelle';
+    let y: number[] = [2, 3, 56];
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            The value {firstValue} is of {typeof firstValue} type!
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    The value {firstValue} is of {typeof firstValue} type!
+                    The value {y} is of {typeof y} type!
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
